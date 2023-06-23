e6497ec898be76231cf5626e06bd0269c0d49f02
naresh sampara <naresh.sampara@gmail.com>
2023-06-23 14:05:37 +0100

    slightChange

--- finance_calculators.py
+++ finance_calculators.py
@@ -1,6 +1,6 @@
 ''' create a program that allows the user to access two
 different financial calculators: an investment calculator and a home loan
-repayment calculator.'''
+repayment calculator. this is thj'''
 
 import math
 
